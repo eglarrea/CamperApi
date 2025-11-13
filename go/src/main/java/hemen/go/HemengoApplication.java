51d4e4f095fe3fb6a17df39869defef437658cd4
eglarrea <145193679+eglarrea@users.noreply.github.com>
2025-11-13 17:12:33 +0100

    Comment out database configuration loading
    Comment out dotenv loading and system property settings.

--- go/src/main/java/hemen/go/HemengoApplication.java
+++ go/src/main/java/hemen/go/HemengoApplication.java
@@ -9,11 +9,11 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class HemengoApplication {
 
 	public static void main(String[] args) {
-		  Dotenv dotenv = Dotenv.load();
+		  /*Dotenv dotenv = Dotenv.load();
 
 	      System.setProperty("DB_URL", dotenv.get("DB_URL"));
 	      System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-	      System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+	      System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));*/
 	      SpringApplication.run(HemengoApplication.class, args);
 	}
 
