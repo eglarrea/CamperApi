088c3b2a9c51c1b4335cca051587791d74bff911
Egoitz Larrea <you@example.com>
2025-11-13 17:02:56 +0100

    Obtener datos de conexion del fichero .env

--- go/src/main/java/hemen/go/HemengoApplication.java
+++ go/src/main/java/hemen/go/HemengoApplication.java
@@ -3,11 +3,18 @@ package hemen.go;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class HemengoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HemengoApplication.class, args);
+		  Dotenv dotenv = Dotenv.load();
+
+	      System.setProperty("DB_URL", dotenv.get("DB_URL"));
+	      System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+	      System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+	      SpringApplication.run(HemengoApplication.class, args);
 	}
 
 }
