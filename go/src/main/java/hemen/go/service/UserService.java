246a867c13d5730bea7228074cdc6a03e15b091c
Egoitz Larrea <you@example.com>
2025-11-15 11:23:29 +0100

    Pasar a service las llamadas

--- go/src/main/java/hemen/go/service/UserService.java
+++ go/src/main/java/hemen/go/service/UserService.java
@@ -1,8 +1,14 @@
 package hemen.go.service;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import hemen.go.dto.request.RegisterRequest;
 import hemen.go.dto.response.UserDtoResponse;
 import hemen.go.entity.Usuario;
 import hemen.go.repository.UsuarioRepository;
@@ -24,14 +30,20 @@ public class UserService {
 
     /** Repositorio para acceder a los usuarios en la base de datos */
     private final UsuarioRepository usuarioRepository;
+    
+    private final PasswordEncoder passwordEncoder;
+    
+    private final MessageSource messageSource;
 
     /**
      * Constructor con inyección de dependencias.
      *
      * @param usuarioRepository repositorio para acceder a los usuarios.
      */
-    public UserService(UsuarioRepository usuarioRepository) {
+    public UserService(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder,MessageSource messageSource) {
         this.usuarioRepository = usuarioRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.messageSource = messageSource;
     }
 
     /**
@@ -52,7 +64,8 @@ public class UserService {
      */
     public UserDtoResponse findByEmail(String email) {
         Usuario user = usuarioRepository.findByEmailPersona(email)
-                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+                .orElseThrow(() -> new UsernameNotFoundException( messageSource.getMessage(
+                        "error.usuario.no.existe", null, LocaleContextHolder.getLocale())));
 
         return new UserDtoResponse(
                 user.getId(),
@@ -64,6 +77,57 @@ public class UserService {
                 user.getEmailPersona(),
                 user.is_admin(),
                 user.getEmpresa() != null ? user.getEmpresa().getNombreEmpresa() : null
+        );
+    }
+    
+    public Usuario getMyData(String email) {
+    	String mensaje = messageSource.getMessage(
+                "error.usuario.no.existe", null, LocaleContextHolder.getLocale());
+        return usuarioRepository.findByEmailPersona(email)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, mensaje));
+    }
+    
+    public Usuario getUserById(Long id) {
+    	String mensaje = messageSource.getMessage(
+                "error.usuario.no.existe", null, LocaleContextHolder.getLocale());
+    	return usuarioRepository.findById(id)
+                 .orElseThrow(() ->  new ResponseStatusException(HttpStatus.NOT_FOUND, mensaje));
+    }
+    
+    public UserDtoResponse updateUserData(String email, RegisterRequest updatedData) {
+    	
+        Usuario usuario = usuarioRepository.findByEmailPersona(email)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, messageSource.getMessage(
+                        "error.usuario.no.existe", null, LocaleContextHolder.getLocale())));
+
+        // Actualizar solo si el campo no es nulo y ha cambiado
+        if (updatedData.getNombrePersona() != null 
+                && !updatedData.getNombrePersona().equals(usuario.getNombre_persona())) {
+            usuario.setNombre_persona(updatedData.getNombrePersona());
+        }
+
+        if (updatedData.getEmailPersona() != null 
+                && !updatedData.getEmailPersona().equals(usuario.getEmailPersona())) {
+            usuario.setEmailPersona(updatedData.getEmailPersona());
+        }
+
+        // Validar y actualizar contraseña solo si se ha enviado y coincide con confirmación
+        if (updatedData.getPassPersona() != null && !updatedData.getPassPersona().isBlank()) {
+            if (!updatedData.getPassPersona().equals(updatedData.getConfirmPassPersona())) {
+            	
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, messageSource.getMessage(
+                        "user.password.confirm", null, LocaleContextHolder.getLocale()));
+            }
+            String encodedPassword = passwordEncoder.encode(updatedData.getPassPersona());
+            usuario.setPass_persona(encodedPassword);
+        }
+
+        Usuario usua = usuarioRepository.save(usuario);
+
+        return new UserDtoResponse(usua.getId(), usua.getNombre_persona(),	usua.getApellidos_persona(),
+        		usua.getFec_nacimiento_persona(), usua.getDni_persona(), usua.getIban_persona(),
+        		usua.getEmailPersona(),	usua.is_admin(),
+        		usua.getEmpresa() != null ? usua.getEmpresa().getNombreEmpresa() : null
         );
     }
 }
