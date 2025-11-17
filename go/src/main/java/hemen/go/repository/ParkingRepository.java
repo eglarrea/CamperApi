d46b0c432a43a9a36a361aca7c79c61d595346bd
Karmel Idarraga <kidarraga@euro-help.es>
2025-11-17 17:41:33 +0100

    Añadimos nuevos métodos para las fechas de las reservas

--- go/src/main/java/hemen/go/repository/ParkingRepository.java
+++ go/src/main/java/hemen/go/repository/ParkingRepository.java
@@ -1,5 +1,6 @@
 package hemen.go.repository;
 
+import java.sql.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,22 +9,25 @@ import hemen.go.entity.Parking;
 
 public interface ParkingRepository extends JpaRepository<Parking, Long> {
 	
-	List<Parking> findBymunicipio(String municipio);
+	List<Parking> findAllBymunicipio(String municipio);
 	
-	List<Parking> findByTieneElectricidad(boolean tieneElectricidad);
-	List<Parking> findByTieneResiduales(boolean tieneResiduales);
-	List<Parking> findByTieneVips(boolean tieneVips);
-	List<Parking> findByTieneElectricidadAndTieneResiduales(boolean tieneElectricidad, boolean tieneResiduales);
-	List<Parking> findByTieneElectricidadAndTieneVips(boolean tieneElectricidad, boolean tieneVips);
-	List<Parking> findByTieneResidualesAndTieneVips(boolean tieneResiduales, boolean tieneVips);
-	List<Parking> findByTieneElectricidadAndTieneResidualesAndTieneVips(boolean tieneElectricidad,boolean tieneResiduales, boolean tieneVips);
+	List<Parking> findAllByTieneElectricidad(boolean tieneElectricidad);
+	List<Parking> findAllByTieneResiduales(boolean tieneResiduales);
+	List<Parking> findAllByTieneVips(boolean tieneVips);
+	List<Parking> findAllByTieneElectricidadAndTieneResiduales(boolean tieneElectricidad, boolean tieneResiduales);
+	List<Parking> findAllByTieneElectricidadAndTieneVips(boolean tieneElectricidad, boolean tieneVips);
+	List<Parking> findAllByTieneResidualesAndTieneVips(boolean tieneResiduales, boolean tieneVips);
+	List<Parking> findAllByTieneElectricidadAndTieneResidualesAndTieneVips(boolean tieneElectricidad,boolean tieneResiduales, boolean tieneVips);
 	
-	List<Parking> findByMunicipioAndTieneElectricidad(String municipio, boolean tieneElectricidad);
-	List<Parking> findByMunicipioAndTieneResiduales(String municipio, boolean tieneResiduales);
-	List<Parking> findByMunicipioAndTieneVips(String municipio, boolean tienePlazasVip);
-	List<Parking> findByMunicipioAndTieneElectricidadAndTieneResiduales(String municipio, boolean tieneElectricidad, boolean tieneResiduales);
-	List<Parking> findByMunicipioAndTieneElectricidadAndTieneVips(String municipio, boolean tieneElectricidad, boolean tienePlazasVip);
-	List<Parking> findByMunicipioAndTieneResidualesAndTieneVips(String municipio, boolean tieneResiduales, boolean tienePlazasVip);
-	List<Parking> findByMunicipioAndTieneElectricidadAndTieneResidualesAndTieneVips(String municipio, boolean tieneElectricidad,boolean tieneResiduales, boolean tienePlazasVip);
+	List<Parking> findAllByMunicipioAndTieneElectricidad(String municipio, boolean tieneElectricidad);
+	List<Parking> findAllByMunicipioAndTieneResiduales(String municipio, boolean tieneResiduales);
+	List<Parking> findAllByMunicipioAndTieneVips(String municipio, boolean tienePlazasVip);
+	List<Parking> findAllByMunicipioAndTieneElectricidadAndTieneResiduales(String municipio, boolean tieneElectricidad, boolean tieneResiduales);
+	List<Parking> findAllByMunicipioAndTieneElectricidadAndTieneVips(String municipio, boolean tieneElectricidad, boolean tienePlazasVip);
+	List<Parking> findAllByMunicipioAndTieneResidualesAndTieneVips(String municipio, boolean tieneResiduales, boolean tienePlazasVip);
+	List<Parking> findAllByMunicipioAndTieneElectricidadAndTieneResidualesAndTieneVips(String municipio, boolean tieneElectricidad,boolean tieneResiduales, boolean tienePlazasVip);
 	
+	List<Parking> findAllByPlazasReservasFecInicioBetween(Date fechaDesde, Date fechaHasta);
+	List<Parking> findAllByPlazasReservasFecFinBetween(Date fechaDesde, Date fechaHasta);
+	List<Parking> findAllByPlazasReservasFecInicioBetweenOrPlazasReservasFecFinBetween(Date fechaInicioDesde, Date fechaInicioHasta, Date fechaFinDesde, Date fechaFinHasta);
 }
