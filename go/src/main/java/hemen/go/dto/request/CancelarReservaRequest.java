225e1f91eb6055a96bf6340b8393debee374e6f1
Egoitz Larrea <you@example.com>
2025-11-18 12:52:18 +0100

    Cancelar reserva

---
+++ go/src/main/java/hemen/go/dto/request/CancelarReservaRequest.java
@@ -0,0 +1,14 @@
+package hemen.go.dto.request;
+
+public class CancelarReservaRequest {
+
+	    private Long idReserva;
+	    
+		public Long getIdReserva() {
+			return idReserva;
+		}
+		public void setIdReserva(Long idReserva) {
+			this.idReserva = idReserva;
+		}
+	   
+}
