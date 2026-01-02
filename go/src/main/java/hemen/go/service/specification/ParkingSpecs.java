df34f5297ba54f607a37edd29b87e8f763bf0d67
Egoitz Larrea <you@example.com>
2026-01-02 15:59:11 +0100

    Buscar en localidad y provincia que contenga el texto

--- go/src/main/java/hemen/go/service/specification/ParkingSpecs.java
+++ go/src/main/java/hemen/go/service/specification/ParkingSpecs.java
@@ -24,14 +24,14 @@ public class ParkingSpecs {
         return (root, query, cb) ->
             provincia == null || provincia.isBlank()
                 ? cb.conjunction()
-                : cb.equal(cb.lower(root.get("provincia")), provincia.trim().toLowerCase());
+                		: cb.like( cb.lower(root.get("provincia")), "%" + provincia.trim().toLowerCase() + "%" );
     }
 
     public static Specification<Parking> porMunicipio(String municipio) {
         return (root, query, cb) ->
             municipio == null || municipio.isBlank()
                 ? cb.conjunction()
-                : cb.equal(cb.lower(root.get("municipio")), municipio.trim().toLowerCase());
+                		: cb.like( cb.lower(root.get("municipio")), "%" + municipio.trim().toLowerCase() + "%" );
     }
 
     public static Specification<Parking> porActivo(Boolean activo) {
